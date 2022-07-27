cd26f8fd5137a02aad8ba450be618740172c6670
Noah Cohn <noahc42@gmail.com>
2022-07-27 03:50:47 -0700

    Update disco_xform_utils.py

--- disco_xform_utils.py
+++ disco_xform_utils.py
@@ -15,6 +15,7 @@ MIN_ADABINS_AREA=448*448
 @torch.no_grad()
 def transform_image_3d(img_filepath,midas_model,midas_transform,rot_mat=torch.eye(3).unsqueeze(0),translate=(0.,0.,0.00),near=2000,far=20000,fov_deg=114):
     padding_mode='border'
+    device=torch.device("cuda")
     sampling_mode='bicubic'
     midas_weight=0.3
     spherical=False
@@ -24,7 +25,7 @@ def transform_image_3d(img_filepath,midas_model,midas_transform,rot_mat=torch.ey
     use_adabins=midas_weight < 1.0
     if use_adabins:
         # AdaBins
-        infer_helper=InferenceHelper(dataset='nyu',device='cuda')
+        infer_helper=InferenceHelper(dataset='nyu',device=device)
         image_pil_area=w*h
         if image_pil_area > MAX_ADABINS_AREA:
        #     print("if image_pil_area > MAX_ADABINS_AREA:")
@@ -76,19 +77,19 @@ def transform_image_3d(img_filepath,midas_model,midas_transform,rot_mat=torch.ey
     depth_map=np.expand_dims(depth_map,axis=0)
     depth_tensor=torch.from_numpy(depth_map).squeeze().to('cuda')
     pixel_aspect=1.0
-    persp_cam_old=p3d.FoVPerspectiveCameras(near,far,pixel_aspect,fov=fov_deg,degrees=True,device='cuda')
-    persp_cam_new=p3d.FoVPerspectiveCameras(near,far,pixel_aspect,fov=fov_deg,degrees=True,R=rot_mat,T=torch.tensor([translate]),device='cuda')
-    y,x=torch.meshgrid(torch.linspace(-1.,1.,h,dtype=torch.float32,device='cuda'),torch.linspace(-1.,1.,w,dtype=torch.float32,device='cuda'))
-    z=torch.as_tensor(depth_tensor,dtype=torch.float32,device='cuda')
+    persp_cam_old=p3d.FoVPerspectiveCameras(near,far,pixel_aspect,fov=fov_deg,degrees=True,device=device)
+    persp_cam_new=p3d.FoVPerspectiveCameras(near,far,pixel_aspect,fov=fov_deg,degrees=True,R=rot_mat,T=torch.tensor([translate]),device=device)
+    y,x=torch.meshgrid(torch.linspace(-1.,1.,h,dtype=torch.float32,device=device),torch.linspace(-1.,1.,w,dtype=torch.float32,device=device))
+    z=torch.as_tensor(depth_tensor,dtype=torch.float32,device=device)
     xyz_old_world=torch.stack((x.flatten(),y.flatten(),z.flatten()),dim=1)
     xyz_old_cam_xy=persp_cam_old.get_full_projection_transform().transform_points(xyz_old_world)[:,0:2]
     xyz_new_cam_xy=persp_cam_new.get_full_projection_transform().transform_points(xyz_old_world)[:,0:2]
     offset_xy=xyz_new_cam_xy-xyz_old_cam_xy
-    identity_2d_batch=torch.tensor([[1.,0.,0.],[0.,1.,0.]],device='cuda').unsqueeze(0)
+    identity_2d_batch=torch.tensor([[1.,0.,0.],[0.,1.,0.]],device=device).unsqueeze(0)
     coords_2d=torch.nn.functional.affine_grid(identity_2d_batch,[1,1,h,w],align_corners=False)
     offset_coords_2d=coords_2d-torch.reshape(offset_xy,(h,w,2)).unsqueeze(0)
     if spherical:
-        spherical_grid=get_spherical_projection(h,w,torch.tensor([0,0],device='cuda'),-0.4,device='cuda')#align_corners=False
+        spherical_grid=get_spherical_projection(h,w,torch.tensor([0,0],device=device),-0.4,device=device)#align_corners=False
         stage_image=torch.nn.functional.grid_sample(image_tensor.add(1/512-0.0001).unsqueeze(0),offset_coords_2d,mode=sampling_mode,padding_mode=padding_mode,align_corners=True)
         new_image=torch.nn.functional.grid_sample(stage_image,spherical_grid,align_corners=True) #,mode=sampling_mode,padding_mode=padding_mode,align_corners=False)
     else:
@@ -96,8 +97,8 @@ def transform_image_3d(img_filepath,midas_model,midas_transform,rot_mat=torch.ey
     img_pil=torchvision.transforms.ToPILImage()(new_image.squeeze().clamp(0,1.))
     #torch.cuda.empty_cache()
     return img_pil
-def get_spherical_projection(H,W,center,magnitude,device='cuda'):  
-    xx,yy=torch.linspace(-1,1,W,dtype=torch.float32,device='cuda'),torch.linspace(-1,1,H,dtype=torch.float32,device='cuda')  
+def get_spherical_projection(H,W,center,magnitude,device=device):  
+    xx,yy=torch.linspace(-1,1,W,dtype=torch.float32,device=device),torch.linspace(-1,1,H,dtype=torch.float32,device=device)  
     gridy,gridx =torch.meshgrid(yy,xx)
     grid=torch.stack([gridx,gridy],dim=-1)  
     d=center-grid
