9abd31fb476d47778f1f5c145ebc01a7b9f3f7a0
Noah Cohn <noahc42@gmail.com>
2022-07-27 10:23:31 -0700

    Update disco_xform_utils.py

--- disco_xform_utils.py
+++ disco_xform_utils.py
@@ -16,7 +16,7 @@ MIN_ADABINS_AREA=448*448
 device=torch.device("cuda:0")
 
 @torch.no_grad()
-def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_mat=torch.eye(3).unsqueeze(0), translate=(0.,0.,0.00), near=100, far=20000, fov_deg=90, padding_mode='border', sampling_mode='bicubic', midas_weight = 0.3,spherical=False):
+def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_mat=torch.eye(3).unsqueeze(0), translate=(0.,0.,0.00), near=5.0, far=10.0, fov_deg=60, padding_mode='border', sampling_mode='bicubic', midas_weight = 0.3,spherical=False):
     img_pil=Image.open(open(img_filepath,'rb')).convert('RGB')
     w,h=img_pil.size
     image_tensor=torchvision.transforms.functional.to_tensor(img_pil).to(device)
