e2daa364f68982b2f3c214a5840c093dd91dba9f
Noah Cohn <noahc42@gmail.com>
2022-08-10 03:09:57 -0700

    Update disco_xform_utils.py

--- disco_xform_utils.py
+++ disco_xform_utils.py
@@ -15,6 +15,7 @@ MAX_ADABINS_AREA = 500000
 MIN_ADABINS_AREA = 448*448
 
 @torch.no_grad()
+@njit(parallel=True,cache=True)
 def transform_image_3d(img_filepath, midas_model, midas_transform, device, rot_mat=torch.eye(3).unsqueeze(0), translate=(0.,0.,0.0), near=0.2, far=16.0, fov_deg=114, padding_mode='border', sampling_mode='bicubic', midas_weight = 0.3,spherical=False):
     img_pil = Image.open(open(img_filepath, 'rb')).convert('RGB')
     w, h = img_pil.size
