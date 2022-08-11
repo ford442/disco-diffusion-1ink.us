628db0fb4ea078d384c2ba25ef3ad6642a1828f2
Noah Cohn <noahc42@gmail.com>
2022-08-10 19:20:21 -0700

    Update disco_xform_utils.py

--- disco_xform_utils.py
+++ disco_xform_utils.py
@@ -11,7 +11,7 @@ import sys, math
 import cv2
 import torchvision.transforms as T
 device=torch.device('cpu')
-from numba import jit
+from numba import njit
 try:
     from infer import InferenceHelper
 except:
